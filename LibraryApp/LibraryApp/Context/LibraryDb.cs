ed530a6b0c4c0842922355e6f3d0fd4098bf924a
Sososoy <87933620+Sososoy@users.noreply.github.com>
2023-12-20 03:05:21 +0900

    CRUD Action & View 생성

---
+++ LibraryApp/LibraryApp/Context/LibraryDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LibraryApp.Models;
+
+namespace LibraryApp.Context
+{
+    public class LibraryDb : DbContext
+    {
+        public LibraryDb():base("name=DBCS") { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
